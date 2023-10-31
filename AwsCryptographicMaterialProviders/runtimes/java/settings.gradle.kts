6b52ec3939587fb7fdfdd5705bc98d9918fe4612
seebees <ryanemer@amazon.com>
2023-10-31 09:22:24 -0700

    Tests needs everything

--- AwsCryptographicMaterialProviders/runtimes/java/settings.gradle.kts
+++ AwsCryptographicMaterialProviders/runtimes/java/settings.gradle.kts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-rootProject.name = "aws-cryptographic-material-providers"
+rootProject.name = "AwsCryptographicMaterialProviders"
 
 includeBuild(File("../../../StandardLibrary/runtimes/java/")) {
   name = "StandardLibrary"
--- TestVectorsAwsCryptographicMaterialProviders/runtimes/java/settings.gradle.kts
+++ TestVectorsAwsCryptographicMaterialProviders/runtimes/java/settings.gradle.kts
@@ -9,3 +9,31 @@ includeBuild(File("../../../AwsCryptographicMaterialProviders/runtimes/java/")) 
     substitute(module("software.amazon.cryptography:aws-cryptographic-material-providers")).using(project(":"))
   }
 }
+
+includeBuild(File("../../../StandardLibrary/runtimes/java/")) {
+  name = "StandardLibrary"
+  dependencySubstitution {
+    substitute(module("software.amazon.cryptography:StandardLibrary")).using(project(":"))
+  }
+}
+
+includeBuild(File("../../../AwsCryptographyPrimitives/runtimes/java/")) {
+  name = "AwsCryptographyPrimitives"
+  dependencySubstitution {
+    substitute(module("software.amazon.cryptography:AwsCryptographyPrimitives")).using(project(":"))
+  }
+}
+
+includeBuild(File("../../../ComAmazonawsDynamodb/runtimes/java/")) {
+  name = "ComAmazonawsDynamodb"
+  dependencySubstitution {
+    substitute(module("software.amazon.cryptography:ComAmazonawsDynamodb")).using(project(":"))
+  }
+}
+
+includeBuild(File("../../../ComAmazonawsKms/runtimes/java/")) {
+  name = "ComAmazonawsKms"
+  dependencySubstitution {
+    substitute(module("software.amazon.cryptography:ComAmazonawsKms")).using(project(":"))
+  }
+}
