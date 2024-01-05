c3abf26d0fdfa52f07f536473bb1c6109b527146
Jose Corella <joseeduardocorella@gmail.com>
2024-01-04 16:53:03 -0800

    fix(Primitives-NET): correctly support netstandard2.0

--- AwsCryptographyPrimitives/runtimes/net/Extern/AESEncryption.cs
+++ AwsCryptographyPrimitives/runtimes/net/Extern/AESEncryption.cs
@@ -1,9 +1,6 @@
 // Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-using System;
-using System.Linq;
-using System.Security.Cryptography;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Security;
 using Wrappers_Compile;
@@ -37,8 +34,7 @@ namespace AESEncryption
 
             try
             {
-                // System.Security.Cryptography.AesGcm is absent in .NET Framework
-#if NETFRAMEWORK
+                // System.Security.Cryptography.AesGcm is absent in .NET Framework or .NET Standard 2.0
                 var param = new AeadParameters(
                     new KeyParameter(keyBytes),
                     encAlg.dtor_tagLength * 8,
@@ -48,12 +44,6 @@ namespace AESEncryption
                 cipher.Init(true, param);
                 var len = cipher.ProcessBytes(msg.Elements, 0, msg.Elements.Length, ciphertext, 0);
                 cipher.DoFinal(ciphertext, len);  // Append authentication tag
-#else
-                var aesCiphertext = new Span<byte>(ciphertext, 0, plaintextBytes.Length);
-                var tag = new Span<byte>(ciphertext, plaintextBytes.Length, encAlg.dtor_tagLength);
-                var cipher = new AesGcm(keyBytes);
-                cipher.Encrypt(nonceBytes, plaintextBytes, aesCiphertext, tag, aadBytes);
-#endif
                 return Result<_IAESEncryptOutput, _IError>.create_Success(
                     __default.EncryptionOutputFromByteSeq(byteseq.FromArray(ciphertext), encAlg));
             }
@@ -83,7 +73,6 @@ namespace AESEncryption
 
             try
             {
-#if NETFRAMEWORK
                 var ciphertextAndTag = ciphertextBytes.Concat(tagBytes).ToArray();
                 var param = new AeadParameters(
                     new KeyParameter(keyBytes),
@@ -93,10 +82,6 @@ namespace AESEncryption
                 var cipher = CipherUtilities.GetCipher("AES/GCM/NoPadding");
                 cipher.Init(false, param);
                 cipher.DoFinal(ciphertextAndTag, plaintext, 0);
-#else
-                var cipher = new AesGcm(keyBytes);
-                cipher.Decrypt(nonceBytes, ciphertextBytes, tagBytes, plaintext, aadBytes);
-#endif
                 return Result<ibyteseq, _IError>
                     .create_Success(byteseq.FromArray(plaintext));
             }
