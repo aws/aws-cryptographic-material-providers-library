a697760fe41eccff860f9969e6e73e719705f649
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-01-18 17:22:16 -0800

    cleanup

--- AwsCryptographyPrimitives/runtimes/python/test/internaldafny/test_dafny_wrapper.py
+++ AwsCryptographyPrimitives/runtimes/python/test/internaldafny/test_dafny_wrapper.py
@@ -6,14 +6,6 @@ pytest will find and execute the `test_dafny` method below,
 which will execute the `internaldafny_test_executor.py` file in the `dafny` directory.
 """
 
-# TODO-Python-PYTHONPATH: Remove all sys.path.append logic from this file
-import sys
-
-internaldafny_dir = '/'.join(__file__.split("/")[:-1])
-
-sys.path.append(internaldafny_dir + "/extern")
-sys.path.append(internaldafny_dir + "/generated")
-
 # Import modules required for Dafny-generated tests.
 # This is not generated; these must be manually added.
 # These are only imported to populate the PYTHONPATH.
@@ -21,6 +13,14 @@ sys.path.append(internaldafny_dir + "/generated")
 # and all Dafny-generated imports are fully qualified.
 # TODO-Python-PYTHONPATH: Remove imports to initialize modules' PYTHONPATHs from this file
 import aws_cryptography_primitives
+
+# TODO-Python-PYTHONPATH: Remove all sys.path.append logic from this file
+import sys
+
+internaldafny_dir = '/'.join(__file__.split("/")[:-1])
+
+sys.path.append(internaldafny_dir + "/extern")
+sys.path.append(internaldafny_dir + "/generated")
 
 def test_dafny():
   # Dafny tests are executed when importing `internaldafny_test_executor`
