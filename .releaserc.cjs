074b97e15a91008ec9d849139b79df6989f3e803
Jose Corella <joseeduardocorella@gmail.com>
2024-02-13 13:30:12 -0800

    maybe

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -53,7 +53,10 @@ const Runtimes = {
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-  branches: ["main"],
+  branches: [
+    "main",
+    "dry-*"
+  ],
   repositoryUrl:
     "git@github.com:aws/aws-cryptographic-material-providers-library.git",
   plugins: [
