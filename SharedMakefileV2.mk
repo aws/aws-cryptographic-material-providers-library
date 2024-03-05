34bdad2d3e94ca49854ceae1d1976380eea89b79
Robin Salkeld <salkeldr@amazon.com>
2024-03-05 12:08:57 -0800

    Copy

--- SharedMakefileV2.mk
+++ SharedMakefileV2.mk
@@ -1,9 +1,11 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-# These make targets that are shared
-# between all the projects in this repo.
+# These are make targets that can be shared between all projects
+# that use a common layout.
 # They will only function if executed inside a project directory.
+# See https://github.com/smithy-lang/smithy-dafny/tree/main-1.x/TestModels
+# for examples.
 
 # There are several variables that are used here.
 # The expectation is to define these variables
@@ -24,11 +26,11 @@
 # AWS_SDK_CMD -- the `--aws-sdk` command to generate AWS SDK style interfaces.
 # STD_LIBRARY -- path from this file to the StandardLibrary Dafny project.
 # SMITHY_DEPS -- path from this file to smithy dependencies, such as custom traits.
+# GRADLEW -- the gradlew to use when building Java runtimes.
+#   defaults to $(SMITHY_DAFNY_ROOT)/codegen/gradlew
 
-# This evaluates to the local path _of this file_.
-# This means that these are the project roots
-# that are shared by all libraries in this repo.
-PROJECT_ROOT := $(abspath $(dir $(abspath $(lastword $(MAKEFILE_LIST)))))
+MAX_RESOURCE_COUNT := 10000000
+
 # This evaluates to the path of the current working directory.
 # i.e. The specific library under consideration.
 LIBRARY_ROOT := $(PWD)
@@ -45,27 +47,17 @@ LIBRARY_ROOT := $(PWD)
 # but still build everything in their local library directory.
 SMITHY_MODEL_ROOT := $(LIBRARY_ROOT)/Model
 
-# Later versions of Dafny no longer default to adding "_Compile"
-# to the names of modules when translating.
-# Our target language code still assumes it does,
-# so IF the /compileSuffix option is available in our verion of Dafny
-# we need to provide it.
-# on 4.2.0 running on windows this shell command fails; we should fix this so the right
-# thing happens on the right environment
-# COMPILE_SUFFIX_OPTION_CHECK_EXIT_CODE := $(shell dafny /help | grep -q /compileSuffix; echo $$?)
-# ifeq ($(COMPILE_SUFFIX_OPTION_CHECK_EXIT_CODE), 0)
-# 	COMPILE_SUFFIX_OPTION := -compileSuffix:1
-# else
-# 	COMPILE_SUFFIX_OPTION := -compileSuffix:1
-# endif
-# for now we know this will work across the three environmnets we test in (windows, macos, ubuntu)
-COMPILE_SUFFIX_OPTION := -compileSuffix:1
-
-# The path to the smithy-dafny code generation CLI.
-# Defaults to the copy in the smithy-dafny submodule but can be overridden.
-CODEGEN_CLI_ROOT := $(PROJECT_ROOT)/smithy-dafny/codegen/smithy-dafny-codegen-cli
+CODEGEN_CLI_ROOT := $(SMITHY_DAFNY_ROOT)/codegen/smithy-dafny-codegen-cli
+GRADLEW := $(SMITHY_DAFNY_ROOT)/codegen/gradlew
 
 ########################## Dafny targets
+
+# TODO: This target will not work for projects that use `replaceable` 
+#       module syntax with multiple language targets.
+# It will fail with error:
+# Error: modules 'A' and 'B' both have CompileName 'same.extern.name'
+# We need to come up with some way to verify files per-language.
+# Rewrite this as part of Java implementation of LanguageSpecificLogic TestModel.
 
 # Proof of correctness for the math below
 #  function Z3_PROCESSES(cpus:nat): nat
@@ -149,6 +141,8 @@ clean-dafny-report:
 # Dafny helper targets
 
 # Transpile the entire project's impl
+# For each index file listed in the project Makefile's PROJECT_INDEX variable,
+#   append a `-library:TestModels/$(PROJECT_INDEX) to the transpiliation target
 _transpile_implementation_all: TRANSPILE_DEPENDENCIES=$(patsubst %, -library:$(PROJECT_ROOT)/%, $(PROJECT_INDEX))
 _transpile_implementation_all: transpile_implementation
 
@@ -167,35 +161,56 @@ _transpile_implementation_all: transpile_implementation
 # Since such targets are all shared,
 # this is tractable.
 
-# At this time is is *significatly* faster
-# to give Dafny a single file
-# that includes everything
-# than it is to pass each file to the CLI.
-# ~2m vs ~10s for our large projects.
-# Also the expectation is that verification happens in the `verify` target
+# If the project under transpilation uses `replaceable` modules,
+#   it MUST define a SRC_INDEX variable per language.
+# SRC_INDEX points to the folder containing the `Index.dfy` file for a particular language
+#   that `include`s all of that language's `replaces` modules.
+# This variable's value might look like (ex.) `src/replaces/net` or `src/replaces/java`.
+# If this variable is not provided, assume the project does not have `replaceable` modules,
+#   and look for `Index.dfy` in the `src/` directory.
+transpile_implementation: SRC_INDEX_TRANSPILE=$(if $(SRC_INDEX),$(SRC_INDEX),src)
+# `find` looks for `Index.dfy` files in either V1 or V2-styled project directories (single vs. multiple model files).
 transpile_implementation:
-	find ./dafny/**/src ./src -name 'Index.dfy' | sed -e 's/^/include "/' -e 's/$$/"/' | dafny \
-		-stdin \
-		-noVerify \
-		-vcsCores:$(CORES) \
-		-compileTarget:$(TARGET) \
-		-spillTargetCode:3 \
-		-compile:0 \
-		-optimizeErasableDatatypeWrapper:0 \
-		$(COMPILE_SUFFIX_OPTION) \
-		-unicodeChar:0 \
-		-functionSyntax:3 \
-		-useRuntimeLib \
-		-out $(OUT) \
-		$(if $(strip $(STD_LIBRARY)) , -library:$(PROJECT_ROOT)/$(STD_LIBRARY)/src/Index.dfy, ) \
-		$(TRANSPILE_DEPENDENCIES)
+	find ./dafny/**/$(SRC_INDEX_TRANSPILE)/ ./$(SRC_INDEX_TRANSPILE)/ -name 'Index.dfy' | sed -e 's/^/include "/' -e 's/$$/"/' | dafny \
+        -stdin \
+        -noVerify \
+        -vcsCores:$(CORES) \
+        -compileTarget:$(TARGET) \
+        -spillTargetCode:3 \
+        -compile:0 \
+        -optimizeErasableDatatypeWrapper:0 \
+        -compileSuffix:1 \
+        -unicodeChar:0 \
+        -functionSyntax:3 \
+        -useRuntimeLib \
+        -out $(OUT) \
+        $(if $(strip $(STD_LIBRARY)) , -library:$(PROJECT_ROOT)/$(STD_LIBRARY)/src/Index.dfy, ) \
+        $(TRANSPILE_DEPENDENCIES)
 
+# If the project under transpilation uses `replaceable` modules,
+#   it MUST define a SRC_INDEX variable per language.
+# The purpose and usage of this is described in the `transpile_implementation` comments.
+_transpile_test_all: SRC_INDEX_TRANSPILE=$(if $(SRC_INDEX),$(SRC_INDEX),src)
+# If the project under transpilation uses `replaceable` modules in its tests
+#   it MUST define a TEST_INDEX variable per language.
+# TEST_INDEX points to the folder containing all test files for a particular language.
+# These files should use Dafny `include`s to include the generic test files as well.
+# This variable's value might look like (ex.) `test/replaces/net` or `test/replaces/java`.
+# If this variable is not provided, assume the project does not have `replaceable` modules,
+#   and look for test files in the `test/` directory.
+_transpile_test_all: TEST_INDEX_TRANSPILE=$(if $(TEST_INDEX),$(TEST_INDEX),test)
+# If the Makefile defines DIR_STRUCTURE_V2 (i.e. multiple models/subprojects/services in project), then:
+#   For each of this project's services defined in PROJECT_SERVICES:
+#     append `-library:/path/to/Index.dfy` to the transpile target
+# Else: (i.e. single model/service in project), then:
+#   append `-library:/path/to/Index.dfy` to the transpile target
+_transpile_test_all: TRANSPILE_DEPENDENCIES=$(if ${DIR_STRUCTURE_V2}, $(patsubst %, -library:dafny/%/$(SRC_INDEX_TRANSPILE)/Index.dfy, $(PROJECT_SERVICES)), -library:$(SRC_INDEX_TRANSPILE)/Index.dfy)
 # Transpile the entire project's tests
-_transpile_test_all: TRANSPILE_DEPENDENCIES=$(if ${DIR_STRUCTURE_V2}, $(patsubst %, -library:dafny/%/src/Index.dfy, $(PROJECT_SERVICES)), -library:src/Index.dfy)
 _transpile_test_all: transpile_test
 
+# `find` looks for tests in either V1 or V2-styled project directories (single vs. multiple model files).
 transpile_test:
-	find ./dafny/**/test ./test -name "*.dfy" -name '*.dfy' | sed -e 's/^/include "/' -e 's/$$/"/' | dafny \
+	find ./dafny/**/$(TEST_INDEX_TRANSPILE) ./$(TEST_INDEX_TRANSPILE) -name "*.dfy" -name '*.dfy' | sed -e 's/^/include "/' -e 's/$$/"/' | dafny \
 		-stdin \
 		-noVerify \
 		-vcsCores:$(CORES) \
@@ -204,14 +219,13 @@ transpile_test:
 		-runAllTests:1 \
 		-compile:0 \
 		-optimizeErasableDatatypeWrapper:0 \
-		$(COMPILE_SUFFIX_OPTION) \
+		-compileSuffix:1 \
 		-unicodeChar:0 \
 		-functionSyntax:3 \
 		-useRuntimeLib \
 		-out $(OUT) \
 		$(if $(strip $(STD_LIBRARY)) , -library:$(PROJECT_ROOT)/$(STD_LIBRARY)/src/Index.dfy, ) \
-		$(TRANSPILE_DEPENDENCIES)
-
+		$(TRANSPILE_DEPENDENCIES) \
 
 # If we are not the StandardLibrary, transpile the StandardLibrary.
 # Transpile all other dependencies
@@ -221,34 +235,54 @@ transpile_dependencies:
 
 ########################## Code-Gen targets
 
-# StandardLibrary is filtered out from dependent-model patsubst list;
-#   Its model is contained in $(LIBRARY_ROOT)/model, not $(LIBRARY_ROOT)/../StandardLibrary/Model.
-
 # The OUTPUT variables are created this way
 # so that it is possible to run _parts_ of polymorph.
 # Otherwise it is difficult to run/test only a Dafny change.
 # Since they are defined per target
 # a single target can decide what parts it wants to build.
 
+# Pass in CODEGEN_CLI_ROOT in command line, e.g.
+#   make polymorph_code_gen CODEGEN_CLI_ROOT=/path/to/smithy-dafny/codegen/smithy-dafny-codegen-cli
+# StandardLibrary is filtered out from dependent-model patsubst list;
+#   Its model is contained in $(LIBRARY_ROOT)/model, not $(LIBRARY_ROOT)/../StandardLibrary/Model.
 _polymorph:
-	@: $(if ${DAFNY_VERSION},,$(error You must pass the value DAFNY_VERSION : DAFNY_VERSION=4.1));
 	cd $(CODEGEN_CLI_ROOT); \
 	./../gradlew run --args="\
 	--dafny-version $(DAFNY_VERSION) \
 	--library-root $(LIBRARY_ROOT) \
 	--patch-files-dir $(if $(DIR_STRUCTURE_V2),$(LIBRARY_ROOT)/codegen-patches/$(SERVICE),$(LIBRARY_ROOT)/codegen-patches) \
-	$(OUTPUT_DAFNY) \
+	--properties-file $(LIBRARY_ROOT)/project.properties \
 	$(INPUT_DAFNY) \
+	$(OUTPUT_DAFNY) \
 	$(OUTPUT_JAVA) \
 	$(OUTPUT_DOTNET) \
+	$(OUTPUT_JAVA) \
 	--model $(if $(DIR_STRUCTURE_V2), $(LIBRARY_ROOT)/dafny/$(SERVICE)/Model, $(SMITHY_MODEL_ROOT)) \
 	--dependent-model $(PROJECT_ROOT)/$(SMITHY_DEPS) \
 	$(patsubst %, --dependent-model $(PROJECT_ROOT)/%/Model, $($(service_deps_var))) \
 	--namespace $($(namespace_var)) \
-	$(AWS_SDK_CMD) \
 	$(OUTPUT_LOCAL_SERVICE_$(SERVICE)) \
+	$(AWS_SDK_CMD) \
 	$(POLYMORPH_OPTIONS) \
 	";
+
+_polymorph_wrapped:
+	@: $(if ${CODEGEN_CLI_ROOT},,$(error You must pass the path CODEGEN_CLI_ROOT: CODEGEN_CLI_ROOT=/path/to/smithy-dafny/codegen/smithy-dafny-codegen-cli));
+	cd $(CODEGEN_CLI_ROOT); \
+	./../gradlew run --args="\
+	--dafny-version $(DAFNY_VERSION) \
+	--library-root $(LIBRARY_ROOT) \
+	--properties-file $(LIBRARY_ROOT)/project.properties \
+	$(OUTPUT_DAFNY_WRAPPED) \
+	$(OUTPUT_DOTNET_WRAPPED) \
+	$(OUTPUT_JAVA_WRAPPED) \
+	--model $(if $(DIR_STRUCTURE_V2),$(LIBRARY_ROOT)/dafny/$(SERVICE)/Model,$(LIBRARY_ROOT)/Model) \
+	--dependent-model $(PROJECT_ROOT)/$(SMITHY_DEPS) \
+	$(patsubst %, --dependent-model $(PROJECT_ROOT)/%/Model, $($(service_deps_var))) \
+	--namespace $($(namespace_var)) \
+	--local-service-test \
+	$(AWS_SDK_CMD) \
+	$(POLYMORPH_OPTIONS)";
 
 _polymorph_dependencies:
 	@$(foreach dependency, \
@@ -269,11 +303,11 @@ polymorph_code_gen:
 	done
 
 _polymorph_code_gen: OUTPUT_DAFNY=\
-	--output-dafny $(if $(DIR_STRUCTURE_V2), $(LIBRARY_ROOT)/dafny/$(SERVICE)/Model, $(LIBRARY_ROOT)/Model)
+    --output-dafny $(if $(DIR_STRUCTURE_V2), $(LIBRARY_ROOT)/dafny/$(SERVICE)/Model, $(LIBRARY_ROOT)/Model)
 _polymorph_code_gen: INPUT_DAFNY=\
-	--include-dafny $(PROJECT_ROOT)/$(STD_LIBRARY)/src/Index.dfy
+		--include-dafny $(PROJECT_ROOT)/$(STD_LIBRARY)/src/Index.dfy
 _polymorph_code_gen: OUTPUT_DOTNET=\
-	$(if $(DIR_STRUCTURE_V2), --output-dotnet $(LIBRARY_ROOT)/runtimes/net/Generated/$(SERVICE)/, --output-dotnet $(LIBRARY_ROOT)/runtimes/net/Generated/)
+    $(if $(DIR_STRUCTURE_V2), --output-dotnet $(LIBRARY_ROOT)/runtimes/net/Generated/$(SERVICE)/, --output-dotnet $(LIBRARY_ROOT)/runtimes/net/Generated/)
 _polymorph_code_gen: OUTPUT_JAVA=--output-java $(LIBRARY_ROOT)/runtimes/java/src/main/smithy-generated
 _polymorph_code_gen: _polymorph
 
@@ -293,9 +327,9 @@ polymorph_dafny:
 	done
 
 _polymorph_dafny: OUTPUT_DAFNY=\
-	--output-dafny $(if $(DIR_STRUCTURE_V2), $(LIBRARY_ROOT)/dafny/$(SERVICE)/Model, $(LIBRARY_ROOT)/Model)
+		--output-dafny $(if $(DIR_STRUCTURE_V2), $(LIBRARY_ROOT)/dafny/$(SERVICE)/Model, $(LIBRARY_ROOT)/Model)
 _polymorph_dafny: INPUT_DAFNY=\
-	--include-dafny $(PROJECT_ROOT)/$(STD_LIBRARY)/src/Index.dfy
+		--include-dafny $(PROJECT_ROOT)/$(STD_LIBRARY)/src/Index.dfy
 _polymorph_dafny: _polymorph
 
 # Generates dotnet code for all namespaces in this project
@@ -329,14 +363,21 @@ polymorph_java:
 _polymorph_java: OUTPUT_JAVA=--output-java $(LIBRARY_ROOT)/runtimes/java/src/main/smithy-generated
 _polymorph_java: _polymorph
 
+# Dependency for formatting generating Java code
+setup_prettier:
+	npm i --no-save prettier@3 prettier-plugin-java@2.5
+
 ########################## .NET targets
 
 transpile_net: | transpile_implementation_net transpile_test_net transpile_dependencies_net
 
 transpile_implementation_net: TARGET=cs
 transpile_implementation_net: OUT=runtimes/net/ImplementationFromDafny
+transpile_implementation_net: SRC_INDEX=$(NET_SRC_INDEX)
 transpile_implementation_net: _transpile_implementation_all
 
+transpile_test_net: SRC_INDEX=$(NET_SRC_INDEX)
+transpile_test_net: TEST_INDEX=$(NET_TEST_INDEX)
 transpile_test_net: TARGET=cs
 transpile_test_net: OUT=runtimes/net/tests/TestsFromDafny
 transpile_test_net: _transpile_test_all
@@ -344,13 +385,13 @@ transpile_test_net: _transpile_test_all
 transpile_dependencies_net: LANG=net
 transpile_dependencies_net: transpile_dependencies
 
-test_net_mac_brew: FRAMEWORK=net6.0
+test_net: FRAMEWORK=net6.0
 test_net:
 	dotnet run \
 		--project runtimes/net/tests/ \
 		--framework $(FRAMEWORK)
 
-test_net_mac_brew: FRAMEWORK=net6.0
+test_net_mac_intel: FRAMEWORK=net6.0
 test_net_mac_intel:
 	DYLD_LIBRARY_PATH="/usr/local/opt/openssl@1.1/lib" dotnet run \
 		--project runtimes/net/tests/ \
@@ -374,7 +415,7 @@ format_net-check:
 ########################## Java targets
 
 build_java: transpile_java mvn_local_deploy_dependencies
-	./runtimes/java/gradlew -p runtimes/java build
+	$(GRADLEW) -p runtimes/java build
 
 transpile_java: | transpile_implementation_java transpile_test_java transpile_dependencies_java
 
@@ -408,18 +449,20 @@ mvn_local_deploy_dependencies:
 
 # The Java MUST all exist already through the transpile step.
 mvn_local_deploy:
-	./runtimes/java/gradlew -p runtimes/java publishMavenLocalPublicationToMavenLocal 
-
-# The Java MUST all exsist if we want to publish to CodeArtifact
-mvn_ca_deploy:
-	./runtimes/java/gradlew -p runtimes/java publishMavenPublicationToPublishToCodeArtifactCIRepository
-
-mvn_staging_deploy:
-	./runtimes/java/gradlew -p runtimes/java publishMavenPublicationToPublishToCodeArtifactStagingRepository
+	$(GRADLEW) -p runtimes/java publishMavenLocalPublicationToMavenLocal
 
 test_java:
-    # run Dafny generated tests
-	./runtimes/java/gradlew -p runtimes/java runTests
+	$(GRADLEW) -p runtimes/java runTests
+
+_clean:
+	rm -f $(LIBRARY_ROOT)/Model/*Types.dfy $(LIBRARY_ROOT)/Model/*TypesWrapped.dfy
+	rm -f $(LIBRARY_ROOT)/runtimes/net/ImplementationFromDafny.cs
+	rm -f $(LIBRARY_ROOT)/runtimes/net/tests/TestFromDafny.cs
+	rm -rf $(LIBRARY_ROOT)/TestResults
+	rm -rf $(LIBRARY_ROOT)/runtimes/net/Generated $(LIBRARY_ROOT)/runtimes/net/bin $(LIBRARY_ROOT)/runtimes/net/obj
+	rm -rf $(LIBRARY_ROOT)/runtimes/net/tests/bin $(LIBRARY_ROOT)/runtimes/net/tests/obj
+
+clean: _clean
 
 ########################## local testing targets
 
