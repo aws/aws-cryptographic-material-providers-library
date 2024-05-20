86bea465b00f169301e70e33f0e01e7669fce3c2
texastony <5892063+texastony@users.noreply.github.com>
2024-05-20 10:23:09 -0700

    fix: revert changelog/version bump in feat PR

--- StandardLibrary/runtimes/net/AssemblyInfo.cs
+++ StandardLibrary/runtimes/net/AssemblyInfo.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 [assembly: AssemblyTitle("AWS.Cryptography.Internal.StandardLibrary")]
 
 // This should be kept in sync with the version number in STD.csproj
-[assembly: AssemblyVersion("1.4.0")]
+[assembly: AssemblyVersion("1.3.0")]
