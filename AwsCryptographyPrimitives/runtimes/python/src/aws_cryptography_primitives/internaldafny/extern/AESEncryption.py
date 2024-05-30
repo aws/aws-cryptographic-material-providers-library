01b8ea58def97b9a8f21e48efc9d7b4b0b916172
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-05-30 13:47:45 -0700

    sync

--- AwsCryptographyPrimitives/runtimes/python/src/aws_cryptography_primitives/internaldafny/extern/AESEncryption.py
+++ AwsCryptographyPrimitives/runtimes/python/src/aws_cryptography_primitives/internaldafny/extern/AESEncryption.py
@@ -1,3 +1,5 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from cryptography.exceptions import InvalidTag
 
