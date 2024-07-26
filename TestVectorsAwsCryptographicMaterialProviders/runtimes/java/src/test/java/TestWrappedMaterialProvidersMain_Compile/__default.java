5d440493d4ab55ec594928a3d3effea41742b1a3
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-07-26 14:12:08 -0700

    missing

---
+++ TestVectorsAwsCryptographicMaterialProviders/runtimes/java/src/test/java/TestWrappedMaterialProvidersMain_Compile/__default.java
@@ -0,0 +1,10 @@
+package TestWrappedMaterialProvidersMain_Compile;
+
+import dafny.DafnySequence;
+
+public class __default extends _ExternBase___default {
+
+  public static dafny.DafnySequence<? extends Character> GetTestVectorExecutionDirectory() {
+    return DafnySequence.asString("");
+  }
+}
