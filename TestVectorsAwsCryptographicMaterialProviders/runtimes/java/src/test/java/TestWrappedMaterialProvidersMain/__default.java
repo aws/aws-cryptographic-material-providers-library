329fcc391ab2a7c117976f4ca95be50331721b59
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-07-26 12:29:43 -0700

    prettier

--- TestVectorsAwsCryptographicMaterialProviders/runtimes/java/src/test/java/TestWrappedMaterialProvidersMain/__default.java
+++ TestVectorsAwsCryptographicMaterialProviders/runtimes/java/src/test/java/TestWrappedMaterialProvidersMain/__default.java
@@ -3,7 +3,11 @@ package TestWrappedMaterialProvidersMain;
 import dafny.DafnySequence;
 
 public class __default {
-    public Wrappers.Result<dafny.DafnySequence<? extends Character>, AwsCryptographyMaterialProvidersTestVectorKeysTypes.Error> GetTestVectorExecutionDirectory() {
-        return Wrappers.Result_Success(DafnySequence.fromString(""));
-    }
+
+  public Wrappers.Result<
+    dafny.DafnySequence<? extends Character>,
+    AwsCryptographyMaterialProvidersTestVectorKeysTypes.Error
+  > GetTestVectorExecutionDirectory() {
+    return Wrappers.Result_Success(DafnySequence.fromString(""));
+  }
 }
