148b84da80c8e7d263fc700511b2d170c359d183
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-07-26 14:16:31 -0700

    fix

--- TestVectorsAwsCryptographicMaterialProviders/runtimes/java/src/test/java/TestWrappedMaterialProvidersMain/__default.java
+++ TestVectorsAwsCryptographicMaterialProviders/runtimes/java/src/test/java/TestWrappedMaterialProvidersMain/__default.java
@@ -4,7 +4,9 @@ import dafny.DafnySequence;
 
 public class __default extends _ExternBase___default {
 
-  public static dafny.DafnySequence<? extends Character> GetTestVectorExecutionDirectory() {
+  public static dafny.DafnySequence<
+    ? extends Character
+  > GetTestVectorExecutionDirectory() {
     return DafnySequence.asString("");
   }
 }
