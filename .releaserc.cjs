a6b080608f1bb0a5b0048dc0ba893c071e81cc94
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-07-29 16:37:58 -0700

    chore(release): .releaserc.cjs updates (#551)

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -60,7 +60,7 @@ const Runtimes = {
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-  branches: ["main", "jocorell/add-mpl-version-properties"],
+  branches: ["main"],
   repositoryUrl:
     "git@github.com:aws/aws-cryptographic-material-providers-library.git",
   plugins: [
