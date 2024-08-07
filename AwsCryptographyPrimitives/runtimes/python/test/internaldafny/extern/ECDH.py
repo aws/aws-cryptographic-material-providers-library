e019fa0f58ba527bc92f741231934e249baadab1
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-08-06 21:34:27 -0700

    clean

--- AwsCryptographyPrimitives/runtimes/python/test/internaldafny/extern/ECDH.py
+++ AwsCryptographyPrimitives/runtimes/python/test/internaldafny/extern/ECDH.py
@@ -1,3 +1,5 @@
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 from aws_cryptography_primitives.internaldafny.extern.ECDH import (
     ECCAlgorithms,
     ECCUtils as src_ECCUtils,
