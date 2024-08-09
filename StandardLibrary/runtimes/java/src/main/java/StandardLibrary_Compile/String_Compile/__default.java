21683da43f4a8f8c628eb785c20402998e877e57
Robin Salkeld <salkeldr@amazon.com>
2024-08-09 10:51:32 -0700

    Add thunk to account for legacy name of StandardLibrary.String

---
+++ StandardLibrary/runtimes/java/src/main/java/StandardLibrary_Compile/String_Compile/__default.java
@@ -0,0 +1,29 @@
+package StandardLibrary_Compile.String_Compile;
+
+/**
+ * 
+ */
+public class __default {
+  public __default() {
+  }
+  public static dafny.DafnySequence<? extends java.math.BigInteger> Int2Digits(java.math.BigInteger n, java.math.BigInteger base)
+  {
+    return StandardLibrary_mString_Compile.__default.Int2Digits(n, base);
+  }
+  public static dafny.DafnySequence<? extends Character> Digits2String(dafny.DafnySequence<? extends java.math.BigInteger> digits, dafny.DafnySequence<? extends Character> chars)
+  {
+    return StandardLibrary_mString_Compile.__default.Digits2String(digits, chars);
+  }
+  public static dafny.DafnySequence<? extends Character> Int2String(java.math.BigInteger n, dafny.DafnySequence<? extends Character> chars)
+  {
+    return StandardLibrary_mString_Compile.__default.Int2String(n, chars);
+  }
+  public static dafny.DafnySequence<? extends Character> Base10Int2String(java.math.BigInteger n)
+  {
+    return StandardLibrary_mString_Compile.__default.Base10Int2String(n);
+  }
+  public static dafny.DafnySequence<? extends Character> Base10()
+  {
+    return StandardLibrary_mString_Compile.__default.Base10();
+  }
+}
