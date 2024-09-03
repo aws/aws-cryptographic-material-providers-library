5feb3058416d8ea97b67eb198723f4bb92cc062d
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-09-03 10:12:30 -0700

    m

--- StandardLibrary/runtimes/python/src/smithy_dafny_standard_library/internaldafny/extern/UTF8.py
+++ StandardLibrary/runtimes/python/src/smithy_dafny_standard_library/internaldafny/extern/UTF8.py
@@ -3,6 +3,8 @@ Extern UTF8 encode and decode methods.
 
 Note:
 Python's `.encode()`/`.decode()` handle surrogates with 'surrogatepass'.
+However, the results of 'surrogatepass' does not comply with Dafny's expectations.
+Dafny expects a strict interpretation of Python's Unicode handling.
 Python represents Unicode characters based on their presence in the Basic Multilingual Plane (BMP).
 The BMP includes characters from U+0000 to U+FFFF (or, 0 <= ord(chr) < 65535).
 
@@ -28,8 +30,15 @@ Since "\uD808\uDC00" is outside the BMP, Python internally represents it as "\uD
 >>> s
 '\ud808\udc00'
 ```
+
+However, the `.decode()` method with 'surrogatepass' leaves '\ud808\udc00' as '𒀀',
+which does not match Dafny's expectation of the literal interpretation of this field.
+To correct this, the Decode extern implementation
+re-encodes any characters outside the BMP,
+then decodes them under the expected decoding.
 """
 import _dafny
+import struct
 
 import smithy_dafny_standard_library.internaldafny.generated.UTF8
 from smithy_dafny_standard_library.internaldafny.generated.UTF8 import *
@@ -39,26 +48,91 @@ from smithy_dafny_standard_library.internaldafny.generated.UTF8 import *
 class default__(smithy_dafny_standard_library.internaldafny.generated.UTF8.default__):
 
   @staticmethod
-  def Encode(s):
+  def Encode(dafny_utf16_code_units):
     try:
       return Wrappers.Result_Success(_dafny.Seq(
-          s.VerbatimString(False).encode('utf-16', 'surrogatepass').decode('utf-16').encode('utf-8')
+          default__._strict_tostring(dafny_utf16_code_units).encode('utf-8')
       ))
     # Catch both UnicodeEncodeError and UnicodeDecodeError.
     # The `try` block involves both encoding and decoding.
-    except (UnicodeDecodeError, UnicodeEncodeError):
+    # OverflowError is possibly raised at `_strict_tostring`'s `ord(c).to_bytes`
+    #   if the char `c` is not valid.
+    except (UnicodeDecodeError, UnicodeEncodeError, OverflowError):
       return Wrappers.Result_Failure(_dafny.Seq("Could not encode input to Dafny Bytes."))
     
   @staticmethod
-  def Decode(s):
+  def _strict_tostring(dafny_ascii_string):
+    """
+    Converts a Dafny Seq of unicode-escaped ASCII characters
+    into a string that can be encoded with Python's built-in `.encode('utf-8')`.
+
+    This encoding-decoding allows subsequent UTF8 encodings
+    to handle surrogates as expected by Dafny code.
+
+    This is exactly the `_dafny.string_from_utf_16` method from the DafnyRuntime,
+    except with two changes
+    1)
+    `errors = 'strict'` here,
+    instead of
+    `errors = 'replace'` in the `_dafny.string_from_utf_16` function.
+    `strict` will throw an exception for invalid encodings, allowing us
+    to detect invalid encodings and raise exceptions,
+    while `replace` will fail silently.
+
+    2)
+    big endian
+    :param s:
+    :return:
+    """
+    return b''\
+      .join([c.to_bytes(2, byteorder="big") \
+            if isinstance(c, int) \
+            else ord(c).to_bytes(2, byteorder="big") \
+            for c in dafny_ascii_string])\
+      .decode("utf-16-be", errors = 'strict')
+    
+  @staticmethod
+  def Decode(dafny_utf8):
     try:
-      decoded = bytes(s).decode('utf-8')
-      return Wrappers.Result_Success(_dafny.Seq(
-        decoded
-      ))
-    except (UnicodeDecodeError):
+      utf8_str = bytes(dafny_utf8).decode('utf-8')
+      # out = []
+      # for a in s:
+      #   out.append(a.to_bytes(2, "little"))
+      # out2 = []
+      # for a in out:
+      #   out2.append(a.decode("utf-8"))
+      # utf8_str = ''.join(out2)
+      unicode_escaped_utf8_str = default__._reverse_strict_tostring(utf8_str)
+      return Wrappers.Result_Success(unicode_escaped_utf8_str)
+    # Catch both UnicodeEncodeError and UnicodeDecodeError.
+    # The `try` block involves both encoding and decoding.
+    # ValueError and TypeError are possibly raised at `_reverse_strict_tostring`'s `chr()`.
+    # struct.error is possibly raised at `struct.unpack`.
+    except (UnicodeDecodeError, UnicodeEncodeError, ValueError, TypeError, struct.error):
       return Wrappers.Result_Failure(_dafny.Seq("Could not decode input from Dafny Bytes."))
 
+
+  @staticmethod
+  def _reverse_strict_tostring(utf8_str):
+    """
+    Converts a string into a Dafny Seq of unicode-escaped ASCII characters.
+    This is the inverse of the `_strict_tostring` function in this file.
+    :param s:
+    :return:
+    """
+    utf16_bytes = utf8_str.encode("utf-16-be", errors = "strict")
+    out = []
+    # len(b)/2 is an integer by construction of UTF-16 encoding (2 bytes per encoded character)
+    for i in range(int(len(utf16_bytes)/2)):
+      # Take two consecutive bytes;
+      utf_16_bytepair = utf16_bytes[2*i:2*i+2]
+      # Unpack them into an ordinal representation;
+      packed_bytes = struct.unpack('>H', utf_16_bytepair)
+      # Convert into a character representation;
+      char_representation = chr(packed_bytes[0])
+      # Append to returned string
+      out.append(char_representation)
+    return _dafny.Seq(out)
 
 # Export externs
 smithy_dafny_standard_library.internaldafny.generated.UTF8.default__ = default__
