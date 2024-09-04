b5c753321c4d6a34a44ac75845444e7290a19a3b
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-09-04 12:56:36 -0700

    new externs, new deps

--- StandardLibrary/runtimes/python/src/smithy_dafny_standard_library/internaldafny/extern/UTF8.py
+++ StandardLibrary/runtimes/python/src/smithy_dafny_standard_library/internaldafny/extern/UTF8.py
@@ -95,13 +95,6 @@ class default__(smithy_dafny_standard_library.internaldafny.generated.UTF8.defau
   def Decode(dafny_utf8):
     try:
       utf8_str = bytes(dafny_utf8).decode('utf-8')
-      # out = []
-      # for a in s:
-      #   out.append(a.to_bytes(2, "little"))
-      # out2 = []
-      # for a in out:
-      #   out2.append(a.decode("utf-8"))
-      # utf8_str = ''.join(out2)
       unicode_escaped_utf8_str = default__._reverse_strict_tostring(utf8_str)
       return Wrappers.Result_Success(unicode_escaped_utf8_str)
     # Catch both UnicodeEncodeError and UnicodeDecodeError.
