55e0c8a677c1aa24c1bfe7020efd9dc06b24d478
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-09-16 14:08:22 -0700

    m

--- AwsCryptographicMaterialProviders/runtimes/python/doc/conf.py
+++ AwsCryptographicMaterialProviders/runtimes/python/doc/conf.py
@@ -33,9 +33,9 @@ KEYSTORE_ALIAS_PATH = "aws_cryptographic_materialproviders.keystore"
 def alias_class_names(app, what, name, obj, skip, options):
     # Create an alias for long class names
     if name.startswith(MPL_SMITHYGENERATED_PATH):
-        name.replace(MPL_SMITHYGENERATED_PATH, MPL_ALIAS_PATH)
+        obj.__name__ = name.replace(MPL_SMITHYGENERATED_PATH, MPL_ALIAS_PATH)
     if name.startswith(KEYSTORE_SMITHYGENERATED_PATH):
-        name.replace(KEYSTORE_SMITHYGENERATED_PATH, KEYSTORE_ALIAS_PATH)
+        obj.__name__ = name.replace(KEYSTORE_SMITHYGENERATED_PATH, KEYSTORE_ALIAS_PATH)
     return skip
 
 def setup(app):
