98311f8c5f72b0130c60012d7cb03d2f5122b79d
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-09-16 13:09:56 -0700

    wip

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -165,7 +165,7 @@ module.exports = {
                 files: [file],
                 from: "{path =.*",
                 to: "\"~${nextRelease.version}\"",
-                results: [CheckDependencyReplacementResults(file)],
+                results: [CheckResults(file)],
                 countMatches: true,
               })),
           ),
