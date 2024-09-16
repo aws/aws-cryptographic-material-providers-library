761975543aa043009ae7930be4b7c4bab0f04601
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-09-16 13:15:22 -0700

    wip

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -200,7 +200,7 @@ function CheckResults(file) {
 
 function CheckDependencyReplacementResults(file) {
   console.log(file);
-  console.log(file.includes("AwsCryptographicMaterialProviders");
+  console.log(file.includes("AwsCryptographicMaterialProviders"));
   if (file.includes("AwsCryptographicMaterialProviders")) {
     return {
       file,
