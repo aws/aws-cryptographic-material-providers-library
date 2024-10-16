576a4ec183b565e0117bf58b4bebe3d80c02bb17
seebees <ryanemer@amazon.com>
2024-10-16 16:30:24 -0700

    CLI warning for `doo` is OK

--- SharedMakefileV2.mk
+++ SharedMakefileV2.mk
@@ -15,11 +15,11 @@ verify:DAFNY_OPTIONS=--allow-deprecation --allow-external-contracts
 verify_single:DAFNY_OPTIONS=--allow-deprecation --allow-external-contracts
 verify_service:DAFNY_OPTIONS=--allow-deprecation --allow-external-contracts
 
-transpile_implementation_net: DAFNY_OPTIONS=--allow-deprecation --compile-suffix --legacy-module-names --allow-external-contracts
-transpile_test_net: DAFNY_OPTIONS=--allow-deprecation --include-test-runner --compile-suffix --legacy-module-names --allow-external-contracts
+transpile_implementation_net: DAFNY_OPTIONS=--allow-warnings --compile-suffix --legacy-module-names --allow-external-contracts
+transpile_test_net: DAFNY_OPTIONS=--allow-warnings --include-test-runner --compile-suffix --legacy-module-names --allow-external-contracts
 
-transpile_implementation_java: DAFNY_OPTIONS=--allow-deprecation --compile-suffix --legacy-data-constructors --legacy-module-names --allow-external-contracts
-transpile_test_java: DAFNY_OPTIONS=--allow-deprecation --include-test-runner --compile-suffix --legacy-data-constructors --legacy-module-names --allow-external-contracts
+transpile_implementation_java: DAFNY_OPTIONS=--allow-warnings --compile-suffix --legacy-data-constructors --legacy-module-names --allow-external-contracts
+transpile_test_java: DAFNY_OPTIONS=--allow-warnings --include-test-runner --compile-suffix --legacy-data-constructors --legacy-module-names --allow-external-contracts
 
-transpile_implementation_python: DAFNY_OPTIONS=--allow-deprecation --allow-external-contracts
-transpile_test_python: DAFNY_OPTIONS=--allow-deprecation --include-test-runner --allow-external-contracts
+transpile_implementation_python: DAFNY_OPTIONS=--allow-warnings --allow-external-contracts
+transpile_test_python: DAFNY_OPTIONS=--allow-warnings --include-test-runner --allow-external-contracts
