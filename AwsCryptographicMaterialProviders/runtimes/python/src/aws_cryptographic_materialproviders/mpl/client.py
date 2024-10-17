44e505e4cc4339d59bbe66558d449c1ac3a2d847
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-10-17 13:06:30 -0700

    rename

--- AwsCryptographicMaterialProviders/runtimes/python/src/aws_cryptographic_materialproviders/mpl/client.py
+++
@@ -1,3 +0,0 @@
-# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-from aws_cryptographic_material_providers.smithygenerated.aws_cryptography_materialproviders.client import *
