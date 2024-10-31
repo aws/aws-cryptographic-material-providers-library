838b85d2d62c04f2181deb3e08728a94e261e674
texastony <5892063+texastony@users.noreply.github.com>
2024-10-31 16:16:32 -0700

    Revert "we need something to let us write only a Mutation Index"
    This reverts commit fc1e7868b43db6c3d3913eda26248f825499640d.

--- AwsCryptographicMaterialProviders/runtimes/python/src/aws_cryptographic_materialproviders/smithygenerated/aws_cryptography_keystore/models.py
+++ AwsCryptographicMaterialProviders/runtimes/python/src/aws_cryptographic_materialproviders/smithygenerated/aws_cryptography_keystore/models.py
@@ -2475,7 +2475,7 @@ class WriteInitializeMutationVersionUnknown:
         return f"WriteInitializeMutationVersionUnknown(tag={self.tag})"
 
 
-# Write Initialize Mutation allows Operations to either rotate/version or simply
+# Write Initialize Mutation allows Mutations to either rotate/version or simply
 # mutate the Active.
 WriteInitializeMutationVersion = Union[
     WriteInitializeMutationVersionRotate,
@@ -2663,7 +2663,7 @@ class OverWriteMutationIndex:
         return all(getattr(self, a) == getattr(other, a) for a in attributes)
 
 
-class WriteMutationIndexCreate:
+class WriteInitializeMutationIndexCreate:
     """Information on an in-flight Mutation of a Branch Key."""
 
     def __init__(self, value: MutationIndex):
@@ -2673,22 +2673,22 @@ class WriteMutationIndexCreate:
         return {"create": self.value.as_dict()}
 
     @staticmethod
-    def from_dict(d: Dict[str, Any]) -> "WriteMutationIndexCreate":
+    def from_dict(d: Dict[str, Any]) -> "WriteInitializeMutationIndexCreate":
         if len(d) != 1:
             raise TypeError(f"Unions may have exactly 1 value, but found {len(d)}")
 
-        return WriteMutationIndexCreate(MutationIndex.from_dict(d["create"]))
+        return WriteInitializeMutationIndexCreate(MutationIndex.from_dict(d["create"]))
 
     def __repr__(self) -> str:
-        return f"WriteMutationIndexCreate(value=repr(self.value))"
+        return f"WriteInitializeMutationIndexCreate(value=repr(self.value))"
 
     def __eq__(self, other: Any) -> bool:
-        if not isinstance(other, WriteMutationIndexCreate):
+        if not isinstance(other, WriteInitializeMutationIndexCreate):
             return False
         return self.value == other.value
 
 
-class WriteMutationIndexUpdate:
+class WriteInitializeMutationIndexUpdate:
     """To avoid information loss, overwrites to any itme in the Key Store are
     done conditioned on the old value."""
 
@@ -2699,22 +2699,24 @@ class WriteMutationIndexUpdate:
         return {"update": self.value.as_dict()}
 
     @staticmethod
-    def from_dict(d: Dict[str, Any]) -> "WriteMutationIndexUpdate":
+    def from_dict(d: Dict[str, Any]) -> "WriteInitializeMutationIndexUpdate":
         if len(d) != 1:
             raise TypeError(f"Unions may have exactly 1 value, but found {len(d)}")
 
-        return WriteMutationIndexUpdate(OverWriteMutationIndex.from_dict(d["update"]))
+        return WriteInitializeMutationIndexUpdate(
+            OverWriteMutationIndex.from_dict(d["update"])
+        )
 
     def __repr__(self) -> str:
-        return f"WriteMutationIndexUpdate(value=repr(self.value))"
+        return f"WriteInitializeMutationIndexUpdate(value=repr(self.value))"
 
     def __eq__(self, other: Any) -> bool:
-        if not isinstance(other, WriteMutationIndexUpdate):
+        if not isinstance(other, WriteInitializeMutationIndexUpdate):
             return False
         return self.value == other.value
 
 
-class WriteMutationIndexUnknown:
+class WriteInitializeMutationIndexUnknown:
     """Represents an unknown variant.
 
     If you receive this value, you will need to update your library to
@@ -2730,27 +2732,31 @@ class WriteMutationIndexUnknown:
         return {"SDK_UNKNOWN_MEMBER": {"name": self.tag}}
 
     @staticmethod
-    def from_dict(d: Dict[str, Any]) -> "WriteMutationIndexUnknown":
+    def from_dict(d: Dict[str, Any]) -> "WriteInitializeMutationIndexUnknown":
         if len(d) != 1:
             raise TypeError(f"Unions may have exactly 1 value, but found {len(d)}")
-        return WriteMutationIndexUnknown(d["SDK_UNKNOWN_MEMBER"]["name"])
+        return WriteInitializeMutationIndexUnknown(d["SDK_UNKNOWN_MEMBER"]["name"])
 
     def __repr__(self) -> str:
-        return f"WriteMutationIndexUnknown(tag={self.tag})"
+        return f"WriteInitializeMutationIndexUnknown(tag={self.tag})"
 
 
-# Write Mutation Index allows Operations to either create or update the Index.
-WriteMutationIndex = Union[
-    WriteMutationIndexCreate, WriteMutationIndexUpdate, WriteMutationIndexUnknown
+# Write Initialize Mutation allows Mutations to either create or update the Index.
+WriteInitializeMutationIndex = Union[
+    WriteInitializeMutationIndexCreate,
+    WriteInitializeMutationIndexUpdate,
+    WriteInitializeMutationIndexUnknown,
 ]
 
 
-def _write_mutation_index_from_dict(d: Dict[str, Any]) -> WriteMutationIndex:
+def _write_initialize_mutation_index_from_dict(
+    d: Dict[str, Any]
+) -> WriteInitializeMutationIndex:
     if "create" in d:
-        return WriteMutationIndexCreate.from_dict(d)
+        return WriteInitializeMutationIndexCreate.from_dict(d)
 
     if "update" in d:
-        return WriteMutationIndexUpdate.from_dict(d)
+        return WriteInitializeMutationIndexUpdate.from_dict(d)
 
     raise TypeError(f"Unions may have exactly 1 value, but found {len(d)}")
 
@@ -2760,7 +2766,7 @@ class WriteInitializeMutationInput:
     version: WriteInitializeMutationVersion
     beacon: OverWriteEncryptedHierarchicalKey
     mutation_commitment: MutationCommitment
-    mutation_index: WriteMutationIndex
+    mutation_index: WriteInitializeMutationIndex
 
     def __init__(
         self,
@@ -2769,7 +2775,7 @@ class WriteInitializeMutationInput:
         version: WriteInitializeMutationVersion,
         beacon: OverWriteEncryptedHierarchicalKey,
         mutation_commitment: MutationCommitment,
-        mutation_index: WriteMutationIndex,
+        mutation_index: WriteInitializeMutationIndex,
     ):
         """
         :param active:
@@ -2796,8 +2802,8 @@ class WriteInitializeMutationInput:
         - only one Mutation affects a Branch Key at a time
         - all
         items of a Branch Key are mutated consistently
-        :param mutation_index: Write Mutation Index allows Operations to either create
-        or update the Index.
+        :param mutation_index: Write Initialize Mutation allows Mutations to either
+        create or update the Index.
         """
         self.active = active
         self.version = version
@@ -2825,7 +2831,9 @@ class WriteInitializeMutationInput:
             "mutation_commitment": MutationCommitment.from_dict(
                 d["mutation_commitment"]
             ),
-            "mutation_index": _write_mutation_index_from_dict(d["mutation_index"]),
+            "mutation_index": _write_initialize_mutation_index_from_dict(
+                d["mutation_index"]
+            ),
         }
 
         return WriteInitializeMutationInput(**kwargs)
@@ -2884,7 +2892,7 @@ class WriteInitializeMutationOutput:
 class WriteMutatedVersionsInput:
     items: list[OverWriteEncryptedHierarchicalKey]
     mutation_commitment: MutationCommitment
-    mutation_index: WriteMutationIndex
+    mutation_index: OverWriteMutationIndex
     end_mutation: bool
 
     def __init__(
@@ -2892,7 +2900,7 @@ class WriteMutatedVersionsInput:
         *,
         items: list[OverWriteEncryptedHierarchicalKey],
         mutation_commitment: MutationCommitment,
-        mutation_index: WriteMutationIndex,
+        mutation_index: OverWriteMutationIndex,
         end_mutation: bool,
     ):
         """
@@ -2904,8 +2912,9 @@ class WriteMutatedVersionsInput:
         - only one Mutation affects a Branch Key at a time
         - all
         items of a Branch Key are mutated consistently
-        :param mutation_index: Write Mutation Index allows Operations to either create
-        or update the Index.
+        :param mutation_index: To avoid information loss, overwrites to any itme in the
+        Key Store
+        are done conditioned on the old value.
         """
         self.items = items
         self.mutation_commitment = mutation_commitment
@@ -2929,7 +2938,7 @@ class WriteMutatedVersionsInput:
             "mutation_commitment": MutationCommitment.from_dict(
                 d["mutation_commitment"]
             ),
-            "mutation_index": _write_mutation_index_from_dict(d["mutation_index"]),
+            "mutation_index": OverWriteMutationIndex.from_dict(d["mutation_index"]),
             "end_mutation": d["end_mutation"],
         }
 
--- AwsCryptographicMaterialProviders/runtimes/python/src/aws_cryptographic_materialproviders/smithygenerated/aws_cryptography_keystore/smithy_to_dafny.py
+++ AwsCryptographicMaterialProviders/runtimes/python/src/aws_cryptographic_materialproviders/smithygenerated/aws_cryptography_keystore/smithy_to_dafny.py
@@ -60,14 +60,14 @@ from aws_cryptographic_materialproviders.internaldafny.generated.AwsCryptography
     VersionKeyOutput_VersionKeyOutput as DafnyVersionKeyOutput,
     WriteAtomicMutationInput_WriteAtomicMutationInput as DafnyWriteAtomicMutationInput,
     WriteAtomicMutationOutput_WriteAtomicMutationOutput as DafnyWriteAtomicMutationOutput,
+    WriteInitializeMutationIndex_create,
+    WriteInitializeMutationIndex_update,
     WriteInitializeMutationInput_WriteInitializeMutationInput as DafnyWriteInitializeMutationInput,
     WriteInitializeMutationOutput_WriteInitializeMutationOutput as DafnyWriteInitializeMutationOutput,
     WriteInitializeMutationVersion_mutate,
     WriteInitializeMutationVersion_rotate,
     WriteMutatedVersionsInput_WriteMutatedVersionsInput as DafnyWriteMutatedVersionsInput,
     WriteMutatedVersionsOutput_WriteMutatedVersionsOutput as DafnyWriteMutatedVersionsOutput,
-    WriteMutationIndex_create,
-    WriteMutationIndex_update,
     WriteNewEncryptedBranchKeyInput_WriteNewEncryptedBranchKeyInput as DafnyWriteNewEncryptedBranchKeyInput,
     WriteNewEncryptedBranchKeyOutput_WriteNewEncryptedBranchKeyOutput as DafnyWriteNewEncryptedBranchKeyOutput,
     WriteNewEncryptedBranchKeyVersionInput_WriteNewEncryptedBranchKeyVersionInput as DafnyWriteNewEncryptedBranchKeyVersionInput,
@@ -487,7 +487,7 @@ def aws_cryptography_keystore_WriteInitializeMutationInput(native_input):
         MutationCommitment=aws_cryptographic_materialproviders.smithygenerated.aws_cryptography_keystore.smithy_to_dafny.aws_cryptography_keystore_MutationCommitment(
             native_input.mutation_commitment
         ),
-        MutationIndex=aws_cryptographic_materialproviders.smithygenerated.aws_cryptography_keystore.smithy_to_dafny.aws_cryptography_keystore_WriteMutationIndex(
+        MutationIndex=aws_cryptographic_materialproviders.smithygenerated.aws_cryptography_keystore.smithy_to_dafny.aws_cryptography_keystore_WriteInitializeMutationIndex(
             native_input.mutation_index
         ),
     )
@@ -520,21 +520,21 @@ def aws_cryptography_keystore_WriteInitializeMutationVersion(native_input):
     return WriteInitializeMutationVersion_union_value
 
 
-def aws_cryptography_keystore_WriteMutationIndex(native_input):
+def aws_cryptography_keystore_WriteInitializeMutationIndex(native_input):
     if isinstance(
         native_input,
-        aws_cryptographic_materialproviders.smithygenerated.aws_cryptography_keystore.models.WriteMutationIndexCreate,
+        aws_cryptographic_materialproviders.smithygenerated.aws_cryptography_keystore.models.WriteInitializeMutationIndexCreate,
     ):
-        WriteMutationIndex_union_value = WriteMutationIndex_create(
+        WriteInitializeMutationIndex_union_value = WriteInitializeMutationIndex_create(
             aws_cryptographic_materialproviders.smithygenerated.aws_cryptography_keystore.smithy_to_dafny.aws_cryptography_keystore_MutationIndex(
                 native_input.value
             )
         )
     elif isinstance(
         native_input,
-        aws_cryptographic_materialproviders.smithygenerated.aws_cryptography_keystore.models.WriteMutationIndexUpdate,
+        aws_cryptographic_materialproviders.smithygenerated.aws_cryptography_keystore.models.WriteInitializeMutationIndexUpdate,
     ):
-        WriteMutationIndex_union_value = WriteMutationIndex_update(
+        WriteInitializeMutationIndex_union_value = WriteInitializeMutationIndex_update(
             aws_cryptographic_materialproviders.smithygenerated.aws_cryptography_keystore.smithy_to_dafny.aws_cryptography_keystore_OverWriteMutationIndex(
                 native_input.value
             )
@@ -544,7 +544,7 @@ def aws_cryptography_keystore_WriteMutationIndex(native_input):
             "No recognized union value in union type: " + str(native_input)
         )
 
-    return WriteMutationIndex_union_value
+    return WriteInitializeMutationIndex_union_value
 
 
 def aws_cryptography_keystore_OverWriteMutationIndex(native_input):
@@ -636,7 +636,7 @@ def aws_cryptography_keystore_WriteMutatedVersionsInput(native_input):
         MutationCommitment=aws_cryptographic_materialproviders.smithygenerated.aws_cryptography_keystore.smithy_to_dafny.aws_cryptography_keystore_MutationCommitment(
             native_input.mutation_commitment
         ),
-        MutationIndex=aws_cryptographic_materialproviders.smithygenerated.aws_cryptography_keystore.smithy_to_dafny.aws_cryptography_keystore_WriteMutationIndex(
+        MutationIndex=aws_cryptographic_materialproviders.smithygenerated.aws_cryptography_keystore.smithy_to_dafny.aws_cryptography_keystore_OverWriteMutationIndex(
             native_input.mutation_index
         ),
         EndMutation=native_input.end_mutation,
