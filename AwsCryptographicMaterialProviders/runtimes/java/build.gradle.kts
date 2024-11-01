0351a1c8790eae0b825eadcff8b768d1e0504b94
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-11-01 10:07:51 -0700

    chore(release): 1.7.3 [skip ci] (#936)

--- AwsCryptographicMaterialProviders/runtimes/java/build.gradle.kts
+++ AwsCryptographicMaterialProviders/runtimes/java/build.gradle.kts
@@ -18,7 +18,7 @@ var props = Properties().apply {
 var dafnyVersion = props.getProperty("dafnyVersion")
 
 group = "software.amazon.cryptography"
-version = "1.7.2-SNAPSHOT"
+version = "1.7.3"
 description = "AWS Cryptographic Material Providers Library"
 
 java {
