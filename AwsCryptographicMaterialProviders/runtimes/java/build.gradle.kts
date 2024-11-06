8da790c4e9bf99b8eea5cc08fcaf895da5a87816
Lucas McDonald <lucasmcdonald3@gmail.com>
2024-11-06 12:30:26 -0800

    m

--- AwsCryptographicMaterialProviders/runtimes/java/build.gradle.kts
+++ AwsCryptographicMaterialProviders/runtimes/java/build.gradle.kts
@@ -18,7 +18,7 @@ var props = Properties().apply {
 var dafnyVersion = props.getProperty("dafnyVersion")
 
 group = "software.amazon.cryptography"
-version = "1.7.4"
+version = "1.7.4-SNAPSHOT"
 description = "AWS Cryptographic Material Providers Library"
 
 java {
