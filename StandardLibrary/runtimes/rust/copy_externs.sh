b5cc38b5f0c5b7fe22a560d4229a657c16e06741
Andrew Jewell <107044381+ajewellamz@users.noreply.github.com>
2024-11-15 09:25:11 -0500

    chore: add rust support (#963)
    * chore: add Rust support

---
+++ StandardLibrary/runtimes/rust/copy_externs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -eu
+
+cd $( dirname ${BASH_SOURCE[0]} )
+
+SRC=../../../AwsCryptographicMaterialProviders/runtimes/rust/src/
+
+cp $SRC/concurrent_call.rs src
+cp $SRC/sets.rs src
+cp $SRC/time.rs src
+cp $SRC/uuid.rs src
