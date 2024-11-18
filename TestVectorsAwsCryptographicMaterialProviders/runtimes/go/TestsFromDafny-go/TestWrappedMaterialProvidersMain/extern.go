12ed075bd8c2cd648e2d00dec862e4f7e025d748
rishav-karanjit <karanjitrishav4@gmail.com>
2024-11-18 10:46:42 -0800

    rm redundant

--- TestVectorsAwsCryptographicMaterialProviders/runtimes/go/TestsFromDafny-go/TestWrappedMaterialProvidersMain/extern.go
+++ TestVectorsAwsCryptographicMaterialProviders/runtimes/go/TestsFromDafny-go/TestWrappedMaterialProvidersMain/extern.go
@@ -1,15 +1,12 @@
 package TestWrappedMaterialProvidersMain
 
 import (
-	"fmt"
 	os "os"
 
 	"github.com/dafny-lang/DafnyRuntimeGo/v4/dafny"
-	_dafny "github.com/dafny-lang/DafnyRuntimeGo/v4/dafny"
 )
 
-func GetTestVectorExecutionDirectory() _dafny.Sequence {
+func GetTestVectorExecutionDirectory() dafny.Sequence {
 	cwd, _ := os.Getwd()
-	fmt.Println(dafny.SeqOfChars([]dafny.Char(cwd + "/../")...))
 	return dafny.SeqOfChars([]dafny.Char(cwd + "/../")...)
 }
