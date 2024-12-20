d90bc642fbd25653fe79be98f17ab9f03ff8752d
Andy Jewell <ajewell@amazon.com>
2024-12-20 13:09:34 -0500

    m

--- StandardLibrary/runtimes/python/src/smithy_dafny_standard_library/internaldafny/extern/OsLang.py
+++ StandardLibrary/runtimes/python/src/smithy_dafny_standard_library/internaldafny/extern/OsLang.py
@@ -11,7 +11,7 @@ class default__(smithy_dafny_standard_library.internaldafny.generated.OsLang.def
         sys = platform.system()
         if sys == "Darwin":
             return _dafny.Seq(_dafny.string_of("MacOS"))
-        return _dafny.string_of(sys)
+        return _dafny.Seq(_dafny.string_of(sys))
 
     def GetOsLong():
         return _dafny.Seq(_dafny.string_of(platform.platform()))
