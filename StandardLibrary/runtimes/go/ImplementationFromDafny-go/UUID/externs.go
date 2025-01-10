51f9e6a30ce6ae28727ad9522027e4930bbbaae8
Shubham Chaturvedi <scchatur@amazon.com>
2025-01-10 15:35:08 -0800

    feat: Std Lib re-polymorph

--- StandardLibrary/runtimes/go/ImplementationFromDafny-go/UUID/externs.go
+++ StandardLibrary/runtimes/go/ImplementationFromDafny-go/UUID/externs.go
@@ -1,8 +1,8 @@
 package UUID
 
 import (
+	"github.com/aws/aws-cryptographic-material-providers-library/releases/go/smithy-dafny-standard-library/Wrappers"
 	"github.com/dafny-lang/DafnyRuntimeGo/v4/dafny"
-	"github.com/dafny-lang/DafnyStandardLibGo/Wrappers"
 	"github.com/google/uuid"
 )
 
