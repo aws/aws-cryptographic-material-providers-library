4fc89165ef13ce515f2117204492d5ca9ea3edc8
Andy Jewell <ajewell@amazon.com>
2025-01-26 11:37:36 -0500

    m

--- TestVectorsAwsCryptographicMaterialProviders/runtimes/rust/src/main.rs
+++ TestVectorsAwsCryptographicMaterialProviders/runtimes/rust/src/main.rs
@@ -65,10 +65,13 @@ pub(crate) use crate::implementation_from_dafny::_StormTracker_Compile;
 pub(crate) use crate::implementation_from_dafny::_LocalCMC_Compile;
 pub(crate) use crate::implementation_from_dafny::_TestWrappedMaterialProvidersMain_Compile;
 
-fn main() {
-    println!("Stack size of main : {:?} {:?}", stacker::remaining_stack(), std::env::var("RUST_MIN_STACK"));
+fn main2() {
     let args: Vec<String> = std::env::args().collect();
     let dafny_strings = args.iter().map(|x| dafny_runtime::dafny_runtime_conversions::unicode_chars_false::string_to_dafny_string(&x)).collect::<Vec<_>>();
     let dafny_args = dafny_runtime::Sequence::from_array_owned(dafny_strings);
     crate::implementation_from_dafny::r#_WrappedMaterialProvidersMain_Compile::_default::Main(&dafny_args);
+}
+
+fn main() {
+    std::thread::Builder::new().stack_size(64 * 1024 * 1024).spawn(main2).unwrap().join().unwrap();
 }
