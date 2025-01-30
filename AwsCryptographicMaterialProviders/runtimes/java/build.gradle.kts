98347c045b9bd34d8a4a3a32ca0a3a1fa8d6a7bd
José Corella <39066999+josecorella@users.noreply.github.com>
2025-01-30 11:59:04 -0800

    chore: allow local testing (#1267)

--- AwsCryptographicMaterialProviders/runtimes/java/build.gradle.kts
+++ AwsCryptographicMaterialProviders/runtimes/java/build.gradle.kts
@@ -18,7 +18,7 @@ var props = Properties().apply {
 var dafnyVersion = props.getProperty("dafnyVersion")
 
 group = "software.amazon.cryptography"
-version = "1.7.5"
+version = "1.7.5-SNAPSHOT"
 description = "AWS Cryptographic Material Providers Library"
 
 java {
