ef1b92182477270b9325b43fd7ebbdcc213ab30e
Ritvik Kapila <ritvik.iitd@gmail.com>
2025-01-31 15:17:14 -0800

    update .releaserc.cjs

--- TestVectorsAwsCryptographicMaterialProviders/runtimes/java/build.gradle.kts
+++ TestVectorsAwsCryptographicMaterialProviders/runtimes/java/build.gradle.kts
@@ -68,7 +68,7 @@ repositories {
 dependencies {
     implementation("org.dafny:DafnyRuntime:${dafnyVersion}")
     implementation("software.amazon.smithy.dafny:conversion:0.1.1")
-    implementation("software.amazon.cryptography:aws-cryptographic-material-providers:1.8.0-SNAPSHOT")
+    implementation("software.amazon.cryptography:aws-cryptographic-material-providers:${version}")
     implementation(platform("software.amazon.awssdk:bom:2.25.1"))
     implementation("software.amazon.awssdk:dynamodb")
     implementation("software.amazon.awssdk:dynamodb-enhanced")
