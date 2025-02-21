3f9793b640466bb0c7412be0efe0f5273af883ef
Jose Corella <joseeduardocorella@gmail.com>
2025-02-20 20:47:35 -0800

    bump recursion limit:

--- TestVectorsAwsCryptographicMaterialProviders/runtimes/python/src/aws_cryptography_materialproviders_test_vectors/__init__.py
+++ TestVectorsAwsCryptographicMaterialProviders/runtimes/python/src/aws_cryptography_materialproviders_test_vectors/__init__.py
@@ -1,6 +1,17 @@
 # Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+"""
+MPL's Dafny code parses the TestVectors JSON recursively.
+i.e. GetTests(Json) = (Json[0], GetTests(Json[1:]))
+The MPL Test Vectors generates more than the default recursion of 1000.
+MPL test vectors exceeds Python's recursion limit when parsing the JSON, which needs >1 call per test vector.
+(Other Crypto Tools languages are limited by memory; Python's explicit limit on function calls is unique.)
+When using this internal Crypto Tools TestVectors library, set recursion limit to 10,000.
+(This value is totally arbitrary and should be increased if this isn't enough.)
+"""
+sys.setrecursionlimit(10000)
+
 # Initialize generated Dafny
 from .internaldafny.generated import module_
 
