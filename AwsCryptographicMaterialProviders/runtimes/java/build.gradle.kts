34b594dc6010c381a7e41049bd8930a13eeaa40a
texastony <5892063+texastony@users.noreply.github.com>
2025-03-13 17:25:23 -0700

    chore(Java): version by properties file only

--- AwsCryptographicMaterialProviders/runtimes/java/build.gradle.kts
+++ AwsCryptographicMaterialProviders/runtimes/java/build.gradle.kts
@@ -224,18 +224,18 @@ tasks.test {
     // This will show System.out.println statements
     testLogging.showStandardStreams = true
 
-    testLogging {
-        lifecycle {
-            events = mutableSetOf(org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED, org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED, org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED)
-            exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
-            showExceptions = true
-            showCauses = true
-            showStackTraces = true
-            showStandardStreams = true
-        }
-        info.events = lifecycle.events
-        info.exceptionFormat = lifecycle.exceptionFormat
-    }
+    // testLogging {
+    //     lifecycle {
+    //         events = mutableSetOf(org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED, org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED, org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED)
+    //         exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+    //         showExceptions = true
+    //         showCauses = true
+    //         showStackTraces = true
+    //         showStandardStreams = true
+    //     }
+    //     info.events = lifecycle.events
+    //     info.exceptionFormat = lifecycle.exceptionFormat
+    // }
 
     // See https://github.com/gradle/kotlin-dsl/issues/836
     addTestListener(object : TestListener {
