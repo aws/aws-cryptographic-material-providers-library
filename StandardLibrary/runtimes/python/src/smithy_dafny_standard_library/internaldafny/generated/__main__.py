214bdbb0efbe712230b0e1d63b0ea5d814f339ba
seebees <ryanemer@amazon.com>
2025-03-28 09:20:15 -0700

    Remove runtime information

--- StandardLibrary/runtimes/python/src/smithy_dafny_standard_library/internaldafny/generated/__main__.py
+++
@@ -1,9 +0,0 @@
-# Dafny program <stdin> compiled into Python
-import sys
-from typing import Callable, Any, TypeVar, NamedTuple
-from math import floor
-from itertools import count
-
-import smithy_dafny_standard_library.internaldafny.generated.module_ as module_
-import _dafny as _dafny
-
