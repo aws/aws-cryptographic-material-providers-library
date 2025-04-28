d15f7a593fdd4fff1fb021aa771fd94028028e1c
rishav-karanjit <karanjitrishav4@gmail.com>
2025-04-28 09:58:27 -0400

    Add comments

--- Examples/java/src/main/java/software/amazon/cryptography/example/hierarchy/mutations/MutationKmsSimpleExample.java
+++ Examples/java/src/main/java/software/amazon/cryptography/example/hierarchy/mutations/MutationKmsSimpleExample.java
@@ -8,6 +8,18 @@ import software.amazon.cryptography.keystore.model.HierarchyVersion;
 import software.amazon.cryptography.keystoreadmin.KeyStoreAdmin;
 import software.amazon.cryptography.keystoreadmin.model.*;
 
+// TODO-HV-2-FF: Support KMS simple for HV-1 to HV-1 mutation
+/**
+ * See {@link MutationExample} for an explanation on Mutations.
+ * AwsKmsSimple is the simple option, that uses one KMS Client (and Grant Token list) and supports both hierarchy-versions.
+ * For HV-1, kms:GenerateDataKeyWithoutPlaintext followed by kms:ReEncrypt is used to create branch keys.
+ * For HV-2, plain-text data keys (PDKs) are created locally via the hosts random number generator;
+ * The branch key context (BKC) and the PDK are protected by kms:Encrypt.
+ * For HV-1, to validate a branch key item, kms:ReEncrypt is used.
+ * For HV-2, to validate a branch key item, kms:Decrypt un-wraps the BKC-PDK tuple,
+ * and the client verifies the read BKC against the protected BKC.
+ */
+
 public class MutationKmsSimpleExample {
 
   public static String End2End(
@@ -35,6 +47,7 @@ public class MutationKmsSimpleExample {
       .Identifier(branchKeyId)
       .Strategy(strategy)
       .SystemKey(systemKey)
+      // Versioning in KMS simple is not yet supported.
       .DoNotVersion(true)
       .build();
 
