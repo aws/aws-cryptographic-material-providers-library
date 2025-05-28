bd3124d79214ee2f3a62fbdbb337f0af92fd71be
Tony Knapp <5892063+texastony@users.noreply.github.com>
2025-05-28 15:15:42 -0700

    test(Dafny): BKS Integ Test ensure EncCtx is prefixed/unprefixed as expected (#1544)

--- Examples/java/src/main/java/software/amazon/cryptography/example/hierarchy/KeyStoreProvider.java
+++ Examples/java/src/main/java/software/amazon/cryptography/example/hierarchy/KeyStoreProvider.java
@@ -2,7 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 package software.amazon.cryptography.example.hierarchy;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import software.amazon.awssdk.services.kms.KmsClient;
 import software.amazon.cryptography.example.Fixtures;
 import software.amazon.cryptography.keystore.KeyStore;
 import software.amazon.cryptography.keystore.model.KMSConfiguration;
@@ -11,7 +13,24 @@ import software.amazon.cryptography.keystore.model.MRDiscovery;
 
 public class KeyStoreProvider {
 
+  /**
+   * Branch Key Store for the default test table with the default KMS Client
+   * @param kmsArn If non-null, creates a strict MRK Branch Key Store;
+   *               otherwise, creates a Discovery Key Store.
+   */
   public static KeyStore keyStore(@Nullable String kmsArn) {
+    return keyStore(kmsArn, Fixtures.kmsClientWest2);
+  }
+
+  /**
+   * @param kmsArn If non-null, creates a strict MRK Branch Key Store;
+   *               otherwise, creates a Discovery Key Store.
+   * @param kmsClient KMS Client used by the Key Store
+   */
+  public static KeyStore keyStore(
+    @Nullable String kmsArn,
+    @Nonnull KmsClient kmsClient
+  ) {
     KMSConfiguration kmsConfiguration;
     if (kmsArn != null) {
       kmsConfiguration = KMSConfiguration.builder().kmsMRKeyArn(kmsArn).build();
@@ -30,7 +49,7 @@ public class KeyStoreProvider {
           .ddbClient(Fixtures.ddbClientWest2)
           .ddbTableName(Fixtures.TEST_KEYSTORE_NAME)
           .logicalKeyStoreName(Fixtures.TEST_LOGICAL_KEYSTORE_NAME)
-          .kmsClient(Fixtures.kmsClientWest2)
+          .kmsClient(kmsClient)
           .kmsConfiguration(kmsConfiguration)
           .build()
       )
