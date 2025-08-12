6a14c910f3139b80c572770413e567dd1e4da4de
Shreyes Sridhara <shreyesx@amazon.com>
2025-08-12 12:16:03 -0700

    chore(python): increase number of test vectors

--- TestVectorsAwsCryptographicMaterialProviders/runtimes/python/src/aws_cryptography_materialproviders_test_vectors/__init__.py
+++ TestVectorsAwsCryptographicMaterialProviders/runtimes/python/src/aws_cryptography_materialproviders_test_vectors/__init__.py
@@ -9,10 +9,8 @@ i.e. GetTests(Json) = (Json[0], GetTests(Json[1:]))
 The MPL Test Vectors generates more than the default recursion of 1000.
 MPL test vectors exceeds Python's recursion limit when parsing the JSON, which needs >1 call per test vector.
 (Other Crypto Tools languages are limited by memory; Python's explicit limit on function calls is unique.)
-When using this internal Crypto Tools TestVectors library, set recursion limit to 10,000.
-(This value is totally arbitrary and should be increased if this isn't enough.)
 """
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(100000) #previous recursion limit 10,000
 
 # Initialize generated Dafny
 from .internaldafny.generated import module_
