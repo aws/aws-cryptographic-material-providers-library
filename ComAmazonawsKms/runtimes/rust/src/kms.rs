6c712c72693114d089540211f2b8f074c2c051fd
Jose Corella <jocorell@amazon.com>
2025-09-11 12:43:26 -0700

    refactor and fix failure

--- ComAmazonawsKms/runtimes/rust/src/kms.rs
+++ ComAmazonawsKms/runtimes/rust/src/kms.rs
@@ -5,7 +5,7 @@
 #![deny(nonstandard_style)]
 #![deny(clippy::all)]
 
-use aws_config::{AppName, Region};
+use aws_config::{AppName, Region, SdkConfig};
 use std::sync::LazyLock;
 static DAFNY_TOKIO_RUNTIME: LazyLock<tokio::runtime::Runtime> = LazyLock::new(|| {
     tokio::runtime::Builder::new_multi_thread()
@@ -33,6 +33,7 @@ impl crate::r#software::amazon::cryptography::services::kms::internaldafny::_def
             )),
         };
 
+        let shared_config = Self::AddUserAgentStringToConfig(&shared_config);
         let shared_config = shared_config
             .to_builder()
             .region(Region::new(region))
@@ -57,23 +58,7 @@ impl crate::r#software::amazon::cryptography::services::kms::internaldafny::_def
                 aws_config::BehaviorVersion::latest(),
             )),
         };
-
-        let runtime = format!("Rust");
-        let runtime_msg = dafny_runtime::dafny_runtime_conversions::unicode_chars_false::string_to_dafny_string(&runtime);
-        let user_agent_string = dafny_runtime::dafny_runtime_conversions::unicode_chars_false::dafny_string_to_string(
-            &crate::software::amazon::cryptography::services::kms::internaldafny::_default::DafnyUserAgentSuffix(&runtime_msg)
-        );
-        // To update the user agent string we take the application name and update it.
-        let current_app_name = shared_config.app_name().unwrap_or_default();
-        let updated_app_name = format!("{} {}", current_app_name, user_agent_string);
-
-        let app_name = AppName::new(updated_app_name)
-            .expect("Valid app name");
-        let shared_config = shared_config
-            .to_builder()
-            .app_name(app_name)
-            .build();
-
+        let shared_config = Self::AddUserAgentStringToConfig(&shared_config);
         let inner = aws_sdk_kms::Client::new(&shared_config);
         let client = crate::client::Client { inner };
         let dafny_client = ::dafny_runtime::upcast_object()(::dafny_runtime::object::new(client));
@@ -81,7 +66,33 @@ impl crate::r#software::amazon::cryptography::services::kms::internaldafny::_def
             value: dafny_client,
         })
     }
+    
+    #[allow(non_snake_case)]
+    pub fn AddUserAgentStringToConfig(sdkConfig: &SdkConfig) -> SdkConfig {
+        let runtime = format!("Rust");
+        let runtime_msg = dafny_runtime::dafny_runtime_conversions::unicode_chars_false::string_to_dafny_string(&runtime);
+        let user_agent_string = dafny_runtime::dafny_runtime_conversions::unicode_chars_false::dafny_string_to_string(
+            &crate::software::amazon::cryptography::services::kms::internaldafny::_default::DafnyUserAgentSuffix(&runtime_msg)
+        );
+        // To update the user agent string we take the application name and update it.
+        let current_app_name = sdkConfig.app_name()
+            .map(|app_name| app_name.to_string())
+            .unwrap_or_else(|| "".to_string());
+        let new_app_name = if current_app_name.is_empty() {
+            format!("{}", user_agent_string)
+        } else {
+            format!("{} {} ", current_app_name, user_agent_string)
+        };
 
+        let app_name = AppName::new(new_app_name)
+            .expect("Valid app name");
+        print!("{}", app_name.to_string());
+        let sdkConfig = sdkConfig 
+            .to_builder()
+            .app_name(app_name)
+            .build();
+        sdkConfig
+    }
     #[allow(non_snake_case)]
     pub fn RegionMatch(
         kmsClient: &::dafny_runtime::Object<dyn crate::software::amazon::cryptography::services::kms::internaldafny::types::IKMSClient>,
